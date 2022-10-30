4ba9a8830ce1db69dc3b631782dcbb65897401fc
Miller Arlindo Hall <hall@hey.com>
2022-10-30 18:23:30 -0400

    Create static website for home server

---
+++ server/static-homesite/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Hall house!</h1>
+        <p>We've got some things you might want to see!</p>
+        <ul>
+          <li><a className="App-link" href="http://media.hallhouse.link">Media server</a></li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
