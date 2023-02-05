323e3453d5cc5de205919e0a44f90f3e2e36c7f3
Miller Arlindo Hall <hall@hey.com>
2023-02-05 15:28:25 -0500

    Move ./opt/rbin to ./opt-rbin

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the Hall house!</h1>
+        <p>We've got some things you might want to see!</p>
+        <ul>
+          <li><a className="App-link" href="https://baby.hallhouse.link">BabyBuddy (username: admin/password: admin)</a></li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
